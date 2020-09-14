da15cfba32390a3f928ad858661386a521cd5ef1
lmhgithi <lmhfox@foxmail.com>
2020-09-14 21:57:43 +0800

    [lmh] finish all tests

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -6,9 +8,17 @@ jest.mock("axios");
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    register("mocked username", "mocked password");
+
+    expect(axios.post).toHaveBeenCalled();
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+
+    await expect(
+      register("mocked username", "mocked password")
+    ).rejects.toThrow("wrong username or password");
   });
 });
